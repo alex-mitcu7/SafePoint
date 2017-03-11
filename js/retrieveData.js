fbcf739792eeb7ce422dd077d6cf2685097e9e8f
morbyx <mihai_vrabie@hotmail.com>
2017-03-11 15:41:27 +0200

    Changed requestPoly

--- js/retrieveData.js
+++ js/retrieveData.js
@@ -85,25 +85,53 @@ var requestSpot = function(lat, long) {
 
 var requestPoly = function(point1, point2, point3, point4) {
 
-
-    $.getJSON("https://data.police.uk/api/crimes-street/all-crime", {
-        poly: point1 + ":" + point2 + ":" + point3+ ":" + point4
-    }, function(result , status, jsXHR){
-        var count = 0;
-        while (result[count]) {
-            count++;
+    var url = "https://data.police.uk/api/crimes-street/all-crime" + "?poly=" + point1 + ":" + point2 + ":" + point3 + ":" + point4;
+    var results = create2DArray(5000);
+    $.ajax({
+        async: false,
+        url: url,
+        success: function(result , status, jsXHR){
+            var count = 0;
+            while (result[count]) {
+                count++;
+            }
+            //var results = create2DArray(count);
+            $("body").append("Status " + status + " ");
+            $.each(result, function(i, field){
+                results[i].push(field.category);
+                results[i].push(field.month);
+                results[i].push(field.location.latitude);
+                results[i].push(field.location.longitude);
+                // $("body")
+                //     .append("i: " + i + " ")
+                //     .append("Category " + results[i][0] + " ")
+                //     .append("Date " + results[i][1] + " ")
+                //     .append("Latitude " + results[i][2] + " ")
+                //     .append("Longitude " + results[i][3] + " ")
+            });
         }
-        var results = create2DArray(count);
-        $("body").append("Status " + status + " ");
-        $.each(result, function(i, field){
-            results[i].push(field.category);
-            results[i].push(field.month);
-            results[i].push(field.location.latitude);
-            results[i].push(field.location.longitude);
-        });
     });
-
     return results;
+
+
+    // $.getJSON("https://data.police.uk/api/crimes-street/all-crime", {
+    //     poly: point1 + ":" + point2 + ":" + point3+ ":" + point4
+    // }, function(result , status, jsXHR){
+    //     var count = 0;
+    //     while (result[count]) {
+    //         count++;
+    //     }
+    //     var results = create2DArray(count);
+    //     $("body").append("Status " + status + " ");
+    //     $.each(result, function(i, field){
+    //         results[i].push(field.category);
+    //         results[i].push(field.month);
+    //         results[i].push(field.location.latitude);
+    //         results[i].push(field.location.longitude);
+    //     });
+    // });
+
+    // return results;
 }
 
 var create2DArray = function (rows) {
